4af92ffbe96d0edc1b89305c5305e4411098fe4a
ainfantp <arnau.infante.pinos@everis.com>
2020-11-05 20:55:28 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scrumboard';
+
+  itemsTODO  = ["eat", "take a shower", "work", "drive"];
+  itemsPROGRESS  = ["programming", "wait for food"];
+  itemsREVISION  = ["make bed"];
+  itemsDONE  = ["wake up", "brush my teeth", "have breakfast", "dress up"];
+
+  onDrop(event: CdkDragDrop<string[]>){
+    if(event.previousContainer === event.container){
+      moveItemInArray(event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }else{
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex, event.currentIndex);
+    }
+  }
 }
